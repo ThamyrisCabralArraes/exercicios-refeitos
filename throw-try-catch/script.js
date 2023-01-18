17efd9e32c4771e1737bc7b4ca23965bc7837817
thamyris <thamyriscabral@hotmail.com>
2023-01-18 14:34:11 -0300

    exemplo try/catch

---
+++ throw-try-catch/script.js
@@ -0,0 +1,16 @@
+const verifity = (n1, n2) => {
+  if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    throw new Error('não é um numero');
+  }
+};
+
+const sum = (n1, n2) => {
+  try {
+    verifity(n1, n2);
+    return n1 + n2;
+  } catch (erro) {
+    return erro.message;
+  }
+};
+
+console.log(sum(2, '2'));
