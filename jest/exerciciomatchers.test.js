8e11df63ea9cbb03a461ad7da4bf5d62d9893729
thamyris <thamyriscabral@hotmail.com>
2023-01-20 11:20:35 -0300

    exemplos de test

--- jest/exerciciomatchers.test.js
+++ jest/exerciciomatchers.test.js
@@ -40,12 +40,3 @@ describe('a função printMessage', () => {
     }).toThrow(new Error('objeto inválido'));
   });
 });
-
-describe('a função printMessage', () => {
-  it.todo('cria um item válido');
-  it.todo('utiliza zero como quantidade padrão');
-  it.todo('Lança um erro quando não recebe parâmetros');
-  it.todo('Lança um erro se o nome do item não é uma string');
-  it.todo('Lança um erro se o preço é negativo');
-  it.todo('Lança um erro se o preço é zero');
-});
