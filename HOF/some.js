b41084a9f69cb59ac0382ff1ed16c9a1c3286573
thamyris <thamyriscabral@hotmail.com>
2023-01-25 11:31:06 -0300

    exemplos some

---
+++ HOF/some.js
@@ -0,0 +1,19 @@
+const pessoas = [
+  { nome: 'Ana', cargo: 'Analista' },
+  { nome: 'João', cargo: 'Gerência' },
+  { nome: 'Aline', cargo: 'Analista' },
+  { nome: 'Joana', cargo: 'Gerência' },
+];
+
+console.log(pessoas.some((pessoa) => pessoa.cargo === 'Gerência'));
+
+console.log('......');
+
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('X', listNames)); // false
+
+console.log('......');
