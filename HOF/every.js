9ea3e3b2995781a7b42885027f7ce651dd260890
thamyris <thamyriscabral@hotmail.com>
2023-01-25 11:48:02 -0300

    exemplos every e some

--- HOF/every.js
+++ HOF/every.js
@@ -10,5 +10,20 @@ console.log(verifyGrades);
 
 console.log('......');
 
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+verifyAges = (arr, minAge) => {
+  return arr.every((person) => person.age >= minAge);
+};
+
+console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 14));
+
 console.log('......');
 console.log('......');
