58520d8bda9dac99906d8b769e370926708e2a76
thamyris <thamyriscabral@hotmail.com>
2023-01-30 15:29:26 -0300

    exercicio destructing e rest e spreed

---
+++ HOF/exercicio-spre-rest-obg.js
@@ -0,0 +1,78 @@
+console.log('exercicio1');
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+console.log(rectangles);
+rectangles.forEach((rectangle) => {
+  console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
+});
+
+console.log('exercicio2');
+
+const sum = (...numeros) => numeros.reduce((acc, curr) => acc + curr, 0);
+console.log(sum(4, 5, 6, 5));
+
+console.log('exercicio3');
+
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+const personLikes = ({ name, age, likes, nationality }) =>
+  `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+// Retornos esperados:
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+console.log('exercicio4');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (lista) => {
+  return lista.filter(
+    ({ nationality, bornIn }) => nationality === 'Australian' && bornIn > 1901 && bornIn < 2000,
+  );
+};
+console.log(filterPeople(people));
