d5b1a86c3735ed6bece639a2dd984025f907724e
thamyris <thamyriscabral@hotmail.com>
2023-01-31 15:50:32 -0300

    correçao no exemplo destructuring

--- HOF/array-destructuring.js
+++ HOF/array-destructuring.js
@@ -97,8 +97,6 @@ const greeting = (user = 'pessoa usuária') => console.log(`Welcome ${user}!`);
 greeting(); // Welcome pessoa usuária!
 
 console.log('....');
-const multiply = (number, value = 1) => {
-  return number * value;
-};
+const multiply = (number, value = 1) => number * value;
 
 console.log(multiply(8));
